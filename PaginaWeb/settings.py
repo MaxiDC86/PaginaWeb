330813e5a4df3a69de930904d1f7a34f0636a498
Mdiascorreia <mdiascorreia86@gmail.com>
2021-07-15 17:19:15 -0300

    several changes on setting.py

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -128,8 +128,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 
