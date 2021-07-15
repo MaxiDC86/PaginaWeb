5b55cfaae935adc2589ba2f7969ca0cda9f95241
Mdiascorreia <mdiascorreia86@gmail.com>
2021-07-15 17:43:16 -0300

    static configuration removed from urls.py

--- PaginaWeb/urls.py
+++ PaginaWeb/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,4 +32,4 @@ urlpatterns = [
     path('tienda/',include('tienda.urls')),
 
     path('carro/',include('carro.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] 
