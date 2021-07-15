ce7f43cfdee5798ab558b7b4694254e9b76ea828
Mdiascorreia <mdiascorreia86@gmail.com>
2021-07-15 17:02:07 -0300

    change allowed_host on setting.py

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_=^&eh$0ylxvpxd!#djvt04=-+fp^d$hm03*=jw-#fheuuhj)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['paginawebproject.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
