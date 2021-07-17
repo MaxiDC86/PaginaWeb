4e405822c66e9533c558f21a6f848a2b81c93202
Mdiascorreia <mdiascorreia86@gmail.com>
2021-07-17 12:33:48 -0300

    postgres migration

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -81,12 +81,14 @@ WSGI_APPLICATION = 'PaginaWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = os.environ('DATABASE_URL')
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
