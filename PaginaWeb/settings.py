805579aec25b98e187ea0f3e944088f2c1750768
Mdiascorreia <mdiascorreia86@gmail.com>
2021-07-17 12:41:06 -0300

    postgres migration rolled back

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -81,14 +81,12 @@ WSGI_APPLICATION = 'PaginaWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = os.environ('DATABASE_URL')
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
