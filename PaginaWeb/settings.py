c1909dd12637b7beaae4973007e7ece7f21dbf8f
Mdiascorreia <mdiascorreia86@gmail.com>
2021-07-16 21:24:12 -0300

    runtime.txt created

--- PaginaWeb/settings.py
+++ PaginaWeb/settings.py
@@ -19,14 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_=^&eh$0ylxvpxd!#djvt04=-+fp^d$hm03*=jw-#fheuuhj)z'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = ['paginawebmaxi.herokuapp.com']
+# ALLOWED_HOSTS = ['paginawebmaxi.herokuapp.com']
 
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 # Application definition
 
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'PaginaWeb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
@@ -140,3 +140,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER'] 
+
+# Configure Django App for Heroku
+
+import django_heroku
+
+django_heroku.settings(locals())
