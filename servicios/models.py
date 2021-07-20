b6ac73b872f0c76adfc27a1ea0e3d029d7c649c5
maxidc86 <max-rub@hotmail.com>
2021-07-19 23:24:53 -0300

    servicios.models 2 images extra removed

--- servicios/models.py
+++ servicios/models.py
@@ -7,8 +7,6 @@ class Servicio(models.Model):
     titulo = models.CharField(max_length=50)
     contenido = models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='servicios')
-    imagen2 = models.ImageField(upload_to='servicios')
-    imagen3 = models.ImageField(upload_to='servicios')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now_add=True)
 
