5221324084c5abe5463d546d8253ac121086696d
maxidc86 <max-rub@hotmail.com>
2021-07-24 18:02:28 -0300

    preference_data added to comprar.html for mercadopago

--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from tienda.models import CategoriaProd, Producto
 
+
+# SDK de Mercado Pago
+import mercadopago
+# Agrega credenciales
+sdk = mercadopago.SDK("TEST-1682191286570055-072420-081eaf21dbcddd9e349e4e05abdcad98-97637851")
+
 # Create your views here.
 
 def tienda(request):
@@ -16,3 +22,17 @@ def comprar(request):
     productos = Producto.objects.all()
 
     return render(request,"tienda/comprar.html", {"categoria":categoria, "productos":productos} )
+
+# Crea un ítem en la preferencia
+preference_data = {
+    "items": [
+        {
+            "title": "Mi producto",
+            "quantity": 1,
+            "unit_price": 75.76,
+        }
+    ]
+}
+
+preference_response = sdk.preference().create(preference_data)
+preference = preference_response["response"]
