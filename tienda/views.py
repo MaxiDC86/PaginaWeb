fb3dff1ffc73e024751f634420efbcacb64d74d3
maxidc86 <max-rub@hotmail.com>
2021-07-24 18:43:41 -0300

    files location changed

--- tienda/views.py
+++ tienda/views.py
@@ -22,17 +22,3 @@ def comprar(request):
     productos = Producto.objects.all()
 
     return render(request,"tienda/comprar.html", {"categoria":categoria, "productos":productos} )
-
-# Crea un ítem en la preferencia
-preference_data = {
-    "items": [
-        {
-            "title": "Mi producto",
-            "quantity": 1,
-            "unit_price": 75.76,
-        }
-    ]
-}
-
-preference_response = sdk.preference().create(preference_data)
-preference = preference_response["response"]
