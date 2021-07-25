75231674a9ac42031167e9bf5aabab5b3959c3ef
maxidc86 <max-rub@hotmail.com>
2021-07-24 23:12:43 -0300

    boing back to original html

--- tienda/static/js/index.js
+++ tienda/static/js/index.js
@@ -129,3 +129,17 @@ document.getElementById('go-back').addEventListener('click', function(){
     setTimeout(() => { $('.container__cart').show(500).fadeIn(); }, 500);
 });
 
+//Handle price update
+function updatePrice(){
+    let quantity = document.getElementById('quantity').value;
+    let unitPrice = document.getElementById('unit-price').innerText;
+    let amount = parseInt(unitPrice) * parseInt(quantity);
+    
+    document.getElementById('cart-total').innerText = '$ ' + amount;
+    document.getElementById('summary-price').innerText = '$ ' + unitPrice;
+    document.getElementById('summary-quantity').innerText = quantity;
+    document.getElementById('summary-total').innerText = '$ ' + amount;
+    document.getElementById('amount').value = amount;
+};
+document.getElementById('quantity').addEventListener('change', updatePrice);
+updatePrice();
