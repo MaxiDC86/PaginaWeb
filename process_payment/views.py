8beae2cc1d9080d866ef3756ddc7721a737e90e2
maxidc86 <max-rub@hotmail.com>
2021-07-25 00:11:03 -0300

    preference

--- process_payment/views.py
+++ process_payment/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
+import mercadopago
+sdk = mercadopago.SDK("TEST-1682191286570055-072420-081eaf21dbcddd9e349e4e05abdcad98-97637851")
 
 # Create your views here.
 def process_payment(request):
 
-    print("hello payment")
-    print(request)
+    if request.method =="POST":
+        # Create a preference item
+        preference_data = {
+        "items": [
+        {
+            "title": "My Item",
+            "quantity": 1,
+            "unit_price": 75.76
+            }
+        ]
+        }
 
-    return render(request,"PaginaWebApp/home.html" )
+        preference_response = sdk.preference().create(preference_data)
+        preference = preference_response["response"]
+
+
+    return render(preference)
